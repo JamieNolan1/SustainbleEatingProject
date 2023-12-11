3b7a1ace2ba77bebc897d57f484fa30a5802f93a
JamieNolan1 <116078755+JamieNolan1@users.noreply.github.com>
2023-12-11 19:38:46 +0000

    Add files via upload

--- FarmsApp/src/farmsapp/Farm.java
+++ FarmsApp/src/farmsapp/Farm.java
@@ -23,12 +23,12 @@ public class Farm {
     
     public static void initializeFarms() {
         farmGroups = new Farm[]{
-        new CropFarms(1, "name", "location", "description", "carrots"),
-        new CropFarms(2, "name", "location", "description", "carrots"),
-        new CropFarms(3, "name", "location", "description", "carrots"),
-        new LivestockFarms(4, "name", "location", "description", 100),
-        new LivestockFarms(5, "name", "location", "description", 100),
-        new LivestockFarms(6, "name", "location", "description", 100) };
+        new CropFarms(1, "veggiefriends", "13 farmers road", "this farm opened in 1857, this Fram group has been growing locall and fresh carrpts for generations, a perfect choice for your favourite rabbit food ", "carrots"),
+        new CropFarms(2, "stan and sons", "2 ashfield road", "this farm opened in 1965, Franks family has been growing the best cabbages around for many years, perfecting the trade.", "cabbages"),
+        new CropFarms(3, "Irishpotatoes", "7 cropcircle avenue", "this farm opened in 1898, and has bunchs of fresh healthy potatoes or sweet potatoes, for amazing prices", "potatos"),
+        new LivestockFarms(4, "oinkers", "4 planters road", "this farm opened in 1990, where the workers have been taking care of sheep to be able to produce sweaters as well as grass fed meat", 166),
+        new LivestockFarms(5, "frank and sons farms", "1 ashfield road", "this farm opened in 1965, the cows from this farm are gven long, healthy lifes, and as a result the meat adn milk they produce is not only sustainable but well-sourced aswell ", 45),
+        new LivestockFarms(6, "WholeMeats inc.", "22 farmers road", "this farm opened in 2018, and has always been committed tp providing the best local pig meat possible, and ensuring the happiness of the animals aswell", 137) };
   }
     
   public static void searchFarmById(){
